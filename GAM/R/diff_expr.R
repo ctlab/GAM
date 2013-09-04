7b2f09751e31728b76e41435f666f40bcf5ed68b
Alexey Sergushichev <alsergbox@gmail.com>
2013-09-04 13:34:46 -0500

    P-values IDs conversion in diff_expr.R
    --HG--
    extra : convert_revision : 076aa2bc8362151efaed50b4d77a410f065ba1f5

--- GAM/R/diff_expr.R
+++ GAM/R/diff_expr.R
@@ -1,6 +1,30 @@
 #!/usr/bin/env Rscript
 library(limma)
 
+convert.pval.biomart <- function(pval, from, to, mart) {
+    library(biomaRt)
+    map <- getBM(attributes=c(from, to), filters=from, values=pval$ID, mart=mart)
+    colnames(map) <- c("from", "to")
+    map$to <- as.character(map$to)
+    map <- na.omit(map)
+    convert.pval(pval, map$from, map$to)
+}
+
+convert.pval <- function(pval, from, to) {
+    map <- data.frame(from=from, to=to, stringsAsFactors=F)
+    pval.ext <- merge(map, pval, by.x = "from", by.y = "ID")
+    res <- data.frame(ID=pval.ext[,"to"], pval=pval.ext$pval, logFC=pval.ext$logFC, stringsAsFactors=F)    
+    
+    get_min_pval <- function(id) {
+        subset <- res[res$ID == id,]
+        
+        # :ToDo: more intelligent aggregate function?
+        subset[which.min(subset$pval),]        
+    }
+    
+    res <- do.call("rbind", lapply(unique(res$ID), get_min_pval))
+    res <- res[order(res$pval),]    
+}
 
 normalize_expressions <- function(exprs, zero.rm=T, log2=T, quantile=T) {
     if (zero.rm) {
@@ -61,6 +85,7 @@ diff.expr <- function(exprs, conditions.vector, state1, state2, top=10000, log2=
         res <- res[, c("id", "pval", "log2FoldChange")]
         colnames(res) <- c("ID", "pval", "logFC")
     } else {
+        library(limma)
         log_expression_matrix<-normalize_expressions(expression_matrix, zero.rm=T, log2=log2, quantile=quantile)
         group_names<-classes_vector
         
@@ -89,83 +114,7 @@ diff.expr <- function(exprs, conditions.vector, state1, state2, top=10000, log2=
         fit2<-eBayes(fit2)
         
         f.top<-topTable(fit2, sort.by="B", number=top, resort.by="P")
-        res = data.frame(ID=f.top$ID, pval=f.top$adj.P.Val, logFC=-f.top$logFC)        
+        res = data.frame(ID=f.top$ID, pval=f.top$adj.P.Val, logFC=-f.top$logFC, stringsAsFactors=F)        
     }
     return(res)
-}
-
-
-
-main <-function() {
-    library(optparse)
-    
-    option_list <- list(
-        #    make_option(c("-v", "--verbose"), action="store_true", default=TRUE,
-        #                help="Print extra output [default]"),
-        #    make_option(c("-q", "--quietly"), action="store_false",
-        #                dest="verbose", help="Print little output"),
-        make_option(c("-e", "--expressions"),
-                    dest="expressions_file",
-                    help="File to read expressions from",
-                    metavar="csv file"),
-        make_option(c("-c", "--conditions"),
-                    dest="conditions_file",
-                    help="File to read conditions from",
-                    metavar="csv file"),
-        make_option(c("--deseq"),
-                    dest="deseq",
-                    action="store_true",
-                    default=FALSE,
-                    help="Use DESeq for differential expression (with non-normalized data)"),
-        make_option(c("--log2"),
-                    dest="log2",
-                    action="store_true",
-                    default=FALSE,
-                    help="Use log2 normalization"),
-        make_option(c("--quantile"),
-                    dest="quantile",
-                    action="store_true",
-                    default=FALSE,
-                    help="Use quantile normalization"),
-        make_option(c("-s1", "--state-1"),
-                    dest="state1",
-                    help="First state for differential expression",
-                    metavar="state"),
-        make_option(c("-s2", "--state-2"),
-                    dest="state2",
-                    help="Second state for differential expression",
-                    metavar="state"),
-        make_option(c("-o", "--output-file"),
-                    dest="output_file",
-                    help="Output file",
-                    metavar="file")
-    )
-    
-    opt <- newEmptyObject()
-    opt$expressions_file <- "./data_new_good/Gene.exp.tsv"
-    opt$conditions_file <- "./data_new_good/Gene.conditions.csv"
-    opt$log2=T
-    opt$quantile=T
-    opt$deseq=F
-    opt$state1="MandLPSandIFNg"
-    opt$state2="MandIL4"
-    
-    opt <- parse_args(OptionParser(option_list=option_list))
-    exprs.sep=","
-    
-    if (grepl("tsv$", opt$expressions_file)) {
-        exprs.sep <- "\t"
-    }
-    
-    exprs<-read.csv(file=opt$expressions_file, head=TRUE, row.names=1, sep=exprs.sep)
-    conditions <- read.csv(file=opt$conditions_file, head=TRUE)
-    classes_vector <- conditions$condition[match(colnames(exprs), conditions$sample)]
-    
-    to_write <- diff.expr(exprs=exprs, conditions.vector=classes_vector,
-                          state1=opt$state1, state2=opt$state2,
-                          log2=opt$log2, quantile=opt$quantile,
-                          use.deseq=opt$deseq)
-    
-    write.table(to_write, file=opt$output_file, sep="\t", row.names=F, quote=F)
-    
 }
