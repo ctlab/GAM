2cc25c82cc9637114e9ce3fa2ee1d542d19d64a9
Alexey Sergushichev <alsergbox@gmail.com>
2013-09-11 17:39:44 -0500

    Script for making mouse.macrophages dataset
    --HG--
    extra : convert_revision : 40f7cd332f49cf9e594ae51a81a180744805a91b

---
+++ make_dataset.R
@@ -0,0 +1,40 @@
+gene.exprs.file <- "./data_new_good/Gene.exp.tsv"
+gene.conditions.file <- "./data_new_good/Gene.conditions.csv"
+gene.data.ids="refseq_mrna"
+
+met.exprs.file <- "./data_new_good/MetDataR2.csv"
+met.conditions.file <- "./data_new_good/Met.conditions.csv"
+
+gene.exprs.sep=","; if (grepl("tsv$", gene.exprs.file)) { gene.exprs.sep <- "\t" }
+gene.exprs <- read.csv(file=gene.exprs.file, head=TRUE, row.names=1, sep=gene.exprs.sep)
+gene.conditions <- read.csv(file=gene.conditions.file, head=TRUE)
+gene.conditions.vector <- gene.conditions$condition[match(colnames(gene.exprs), gene.conditions$sample)]
+
+met.exprs.sep=","; if (grepl("tsv$", met.exprs.file)) { met.exprs.sep <- "\t" }
+met.exprs <- read.csv(file=met.exprs.file, head=TRUE, row.names=1, sep=met.exprs.sep)
+met.conditions <- read.csv(file=met.conditions.file, head=TRUE)
+met.conditions.vector <- met.conditions$condition[match(colnames(met.exprs), met.conditions$sample)]
+
+gene.de <- diff.expr(
+    exprs=gene.exprs, conditions.vector=gene.conditions.vector,
+    state1=state1, state2=state2, 
+    log2=F, quantile=F, use.deseq=T, top=10000)
+
+met.de <- diff.expr(
+    exprs=met.exprs, conditions.vector=met.conditions.vector,
+    state1=state1, state2=state2, 
+    log2=F, quantile=F, use.deseq=F, top=Inf)
+
+
+mouse.macrophages <- newEmptyObject()
+mouse.macrophages$gene.exprs <- gene.exprs
+mouse.macrophages$gene.conditions.vector <- gene.conditions.vector
+mouse.macrophages$met.conditions.vector <- met.conditions.vector
+mouse.macrophages$met.exprs <- met.exprs
+mouse.macrophages$state1 <- state1
+mouse.macrophages$state2 <- state2
+mouse.macrophages$gene.de <- gene.de
+mouse.macrophages$met.de <- met.de
+mouse.macrophages$met.ids <- "HMDB"
+mouse.macrophages$gene.ids <- "RefSeq"
+save(mouse.macrophages, file="./GAM/data/mouse.macrophages.rda")
