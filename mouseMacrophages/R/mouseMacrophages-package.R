cd6c48a28a3b0f7063fd86e155ff4fc8fcba06f1
Alexey Sergushichev <alsergbox@gmail.com>
2013-11-06 11:34:09 -0600

    Added package for mouse macrophages data

---
+++ mouseMacrophages/R/mouseMacrophages-package.R
@@ -0,0 +1,18 @@
+#' Package for integrated analyses of genomic and metabolomic data.
+#' 
+#' Detailed description goes here :ToDo:
+#' @name mouseMacrophages-package
+#' @aliases mouseMacrophages
+#' @docType package
+#' @author Abhishek Jha, Ching-Cheng Huang, Alexey Sergushichev, Yulia Ivanova, Karina Chmielewski, Kelly Stewart, Bart Everts, Juliet Ashall, Edward J. Pearce, Edward M. Driggers, Maxim N. Artyomov
+NULL
+
+#' ExpressionSet with gene expressions calculated with DESeq from 3' RNA-seq data
+#' @docType data
+#' @name mmpGeneSet
+NULL
+
+#' Mass spectrometry data for metabolites, log-scaled and normalized.
+#' @docType data
+#' @name mmpMetSet
+NULL
