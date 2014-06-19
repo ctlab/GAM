b306350e3976a97180db5ecd3c2b3e6dcef67520
Alexey Sergushichev <alsergbox@gmail.com>
2014-06-19 06:15:41 -0500

    not adding Irg1 reaction anymore

--- networks/kegg/make_networks.R
+++ networks/kegg/make_networks.R
@@ -1,14 +1,18 @@
 #!/usr/bin/env Rscript
 library(methods)
 library(BioNet)
+library(org.Mm.eg.db)
+library(org.Hs.eg.db)
+
 library("GAM")
 
 load("kegg.db.rda")
 
-kegg.db$net <- rbind(kegg.db$net, c("C00417", "RXIRG1", "C00490", "RPIRG1", "main"))
-kegg.db$rxn2enz <- rbind(kegg.db$rxn2enz, c("RXIRG1", "Irg1"))
-kegg.db$enz2gene <- rbind(kegg.db$enz2gene, c("Irg1", "16365", "MMU"))
-kegg.db$enz2gene <- rbind(kegg.db$enz2gene, c("Irg1", "730249", "HSA"))
+
+#kegg.db$net <- rbind(kegg.db$net, c("C00417", "RXIRG1", "C00490", "RPIRG1", "main"))
+#kegg.db$rxn2enz <- rbind(kegg.db$rxn2enz, c("RXIRG1", "Irg1"))
+#kegg.db$enz2gene <- rbind(kegg.db$enz2gene, c("Irg1", "16365", "MMU"))
+#kegg.db$enz2gene <- rbind(kegg.db$enz2gene, c("Irg1", "730249", "HSA"))
 
 kegg.mouse.network <- makeKeggNetwork(kegg.db, "MMU")
 save(kegg.mouse.network, file="kegg.mouse.network.rda", compress="xz")
