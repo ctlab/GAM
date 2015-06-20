40a6f6225a47794cb1365f7b10a8ac58343eb7bd
Alexey Sergushichev <alsergbox@gmail.com>
2015-06-20 12:10:36 +0300

    Better dependencies

--- install-dependencies.R
+++ install-dependencies.R
@@ -1,2 +1,18 @@
 #!/usr/bin/env Rscript
-install.packages(c("devtools", "roxygen2"))
+
+list.of.packages <- c("devtools", "roxygen2")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+if (length(new.packages) > 0) {
+    install.packages(new.packages)
+}
+
+
+list.of.packages <- c("DESeq")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+if (length(new.packages) > 0) {
+    biocLite(new.packages)
+}
+
+
